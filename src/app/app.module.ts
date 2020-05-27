9320e503f612a2b75d18802daf5f927ffb66f038
Pablo García <pablonic.gar@gmail.com>
2020-05-27 20:36:20 -0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedComponent } from './shared/shared.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetalleInstrumentoComponent } from './components/detalle-instrumento/detalle-instrumento.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { InstrumentosComponent } from './components/instrumentos/instrumentos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SharedComponent,
+    HomeComponent,
+    DetalleInstrumentoComponent,
+    BuscadorComponent,
+    InstrumentosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
