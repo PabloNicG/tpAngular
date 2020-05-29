66e380f57f4ee769c29fe4984360273bae30e157
Pablo García <pablonic.gar@gmail.com>
2020-05-29 19:47:35 -0300

    Service agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-//import { SharedComponent } from './shared/shared.component';
-import { HomeComponent } from './components/home/home.component';
-import { DetalleInstrumentoComponent } from './components/detalle-instrumento/detalle-instrumento.component';
-import { BuscadorComponent } from './components/buscador/buscador.component';
-import { InstrumentosComponent } from './components/instrumentos/instrumentos.component';
+import { SharedComponent } from './components/shared/shared.component';
+// ';
+// import { HomeComponent } from './components/home/home.component';
+// import { DetalleInstrumentoComponent } from './components/detalle-instrumento/detalle-instrumento.component';
+// import { BuscadorComponent } from './components/buscador/buscador.component';
+// import { InstrumentosComponent } from './components/instrumentos/instrumentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //SharedComponent,
-    HomeComponent,
-    DetalleInstrumentoComponent,
-    BuscadorComponent,
-    InstrumentosComponent,
+    AppComponent
+    // HomeComponent,
+    // DetalleInstrumentoComponent,
+    // BuscadorComponent,
+    // InstrumentosComponent,
 
   ],
   imports: [
